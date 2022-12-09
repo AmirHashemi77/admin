fa37e31afa266e8d07e38cfba43228ad8680962c
AmirHashemi <amirhashemi7706@gmail.com>
2022-12-09 14:29:01 +0330

    add routing in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import List from './pages/list/List';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/user' element={<List/>}/>
+      <Route path='/user/:userId' element={<Single/>}/>
+      <Route path='/user/new' element={<New/>}/>
+    </Routes>
   );
 };
 
