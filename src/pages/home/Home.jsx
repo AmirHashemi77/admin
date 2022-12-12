fa914b2dbd26267e35ba222b55ea0ea9e9c85226
AmirHashemi <amirhashemi7706@gmail.com>
2022-12-12 16:46:20 +0330

    add widget section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Sidebar from '../../component/Sidebar/Sidebar';
 import "./home.scss";
 import Navbar from '../../component/Navbar/Navbar';
+import Widget from '../../component/Widget/Widget';
 const Home = () => {
     return (
         <div className='home'> 
             <Sidebar/>
             <div className='home-container'>
                 <Navbar/>
+                <div className='widgets'>
+                    <Widget type='user'/>
+                    <Widget type='order'/>
+                    <Widget type='earning'/>
+                    <Widget type='balance'/>
+                </div>
             </div>
         </div>
     );
