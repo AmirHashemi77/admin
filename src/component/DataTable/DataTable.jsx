d1cd1f50ba8f05189cf93d80febbddb8da6aef33
AmirHashemi <amirhashemi7706@gmail.com>
2022-12-14 12:15:12 +0330

    complete data table

--- src/component/DataTable/DataTable.jsx
+++ src/component/DataTable/DataTable.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns , userRows } from '../../databaseSource';
 import "./dataTable.scss"
+import { Link } from 'react-router-dom';
 const DataTable = () => {
+
+    const actionColumn=[
+        {
+            field: "action",
+            headerName: "Action",
+            width: 230,
+            renderCell: (params) => {
+              return (
+                <div className="cellAction">
+                  <Link to='/user/test' className='view-button'>View</Link>
+                  <button className='delete-button'>Delete</button>
+                  
+                </div>
+              );
+            },
+          }
+    ]
+
     return (
         <div className='dataTable'>
                 <DataGrid
                     rows={userRows}
-                    columns={userColumns}
+                    columns={userColumns.concat(actionColumn)}
                     pageSize={9}
                     rowsPerPageOptions={[5]}
                     checkboxSelection
